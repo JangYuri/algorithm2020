0c07a39a90b28c5bd66b28450f98f7e387ba608f
JangYuri <cyrjang21@gmail.com>
2020-03-05 00:51:37 +0900

    와일드카드 - 메모이제이션 적용, jlis 문제 작성

---
+++ solutsions/jlis/Solution.java
@@ -0,0 +1,49 @@
+package algorithm2020.solutsions.jlis;
+
+import java.util.*;
+
+class Solution {
+    /*
+     * 어떤 수열에서 0개 이상의 숫자를 지운 결과를 원 수열의 부분 수열이라고 부른다. 그 중 중복된 숫자가 없고 오름 차순으로 정렬되어 있는
+     * 부분 수열들을 가리켜 증가 부분 수열이라고 부른다.
+     * 
+     * 두 개의 정수 수열 A와 B에서 각각 길이 0이상의 증가 부분 수열을 얻은 뒤 이들을 크기 순서대로 합친 것을 합친 증가 부분 수열이라고
+     * 부르기로 하자. 이 중 가장 긴 수열을 합친LIS라고 부르자.
+     * 
+     * 예를들어 '1 3 4 7 9'는 '1 9 4'와 '3 4 7'의 합친 LIS이다. '1 9'와 '3 4 7'을 합쳐 '1 3 4 7 9'를
+     * 얻을 수 있기 때문이다. A와 B가 주어질 때, 합친 LIS의 길이를 계산하는 프로그램을 작성하라
+     */
+    public static void main(String args[]) throws Exception {
+        List<TestCase> test = new ArrayList<>();
+        test.add(new TestCase(new int[] { 1, 2, 4 }, new int[] { 3, 4, 7 }, 5));
+        test.add(new TestCase(new int[] { 1, 2, 4 }, new int[] { 4, 5, 6 }, 6));
+        test.add(new TestCase(new int[] { 10, 20, 30, 1, 2 }, new int[] { 10, 20, 30 }, 5));
+
+        for (int i = 0; i < test.size(); i++) {
+            System.out.println("TEST " + (i + 1));
+            System.out.println("예상 정답 : " + test.get(i).answer);
+
+            System.out.println(" * 내 정답 : " + mySolution(test.get(i).A, test.get(i).B));
+        }
+    }
+
+    static int mySolution(int[] a, int[] b) {
+        int answer = 0;
+
+        return answer;
+    }
+
+}
+
+class TestCase {
+    public int[] A;
+    public int[] B;
+    public int answer;
+
+    TestCase(int[] A, int[] B, int answer) {
+        this.A = A;
+        this.B = B;
+        this.answer = answer;
+    }
+
+}
--- solutsions/wildcard/Solution.java
+++ solutsions/wildcard/Solution.java
@@ -33,14 +33,17 @@ class Solution {
     static String[] mySolution(String w, String[] filename) {
         List<String> answer = new ArrayList<>();
         /*
-         * 1. 글자 하나하나 대응시켜서 맞는지 확인 2. w랑 filename이랑 하나하나 맞춰나감 - 더이상 나갈 수 없을 때 종료 1.
-         * s-filename이 대응되지 않을 때 2. w 끝에 도달 - 길이가 정확하게 같아야 (패턴 = 문자열) 3. filename 끝에 도달
-         * - 패턴은 남았는데 문자열이 이미 끝난 경우 - 남은 패턴이 전부 *라면 대응 가능 4. w[pos]가 *인 경우 - 몇글자가 대응될지
-         * 모르기 때문에 재귀호출로 모든 가능성 검사
+         * 1. 글자 하나하나 대응시켜서 맞는지 확인
+         * 
+         * 2. w랑 filename이랑 하나하나 맞춰나감 - 더이상 나갈 수 없을 때 종료
+         * 
+         * 1. s-filename이 대응되지 않을 때 2. w 끝에 도달 - 길이가 정확하게 같아야 (패턴 = 문자열) 3. filename 끝에
+         * 도달 - 패턴은 남았는데 문자열이 이미 끝난 경우 - 남은 패턴이 전부 *라면 대응 가능 4. w[pos]가 *인 경우 - 몇글자가
+         * 대응될지 모르기 때문에 재귀호출로 모든 가능성 검사
          */
 
         for (int i = 0; i < filename.length; i++) {
-            if (isMatch(w, filename[i]))
+            if (isMatch(new HashMap<Integer[], Boolean>(), 0, 0, w, filename[i]))
                 answer.add(filename[i]);
         }
 
@@ -60,6 +63,38 @@ class Solution {
                 if (isMatch(w.substring(pos + 1), s.substring(pos + skip)))
                     return true;
             }
+        return false;
+    }
+
+    /*
+     * 1. 메모이제이션을 이용하여 매번 스트링객체를 쪼개서 만들지 않고, 위치값을 이용하여 탐색하면서 케시에 비교값을 넣고 그 값들을 계속해서
+     * 참조하여 속도를 높인다. >> 자바는 해쉬를 이용해서!! // -1 : 아직 답이 계산되지 않았음을 의미 // 1은 해당 입력들이 서로
+     * 대응됨을 의미 // 0은 해당 입력들이 서로 대응되지 않음을 의미한다. 2. while문에서 스트링 인덱스값만 올려주고 있는데, 여기에서
+     * 바로 그 다음 인덱스 값으로 재귀로 불러주면 더 단순해진다. 에 몇글자가 대응되어야 할지를 확인하는 것도 반복문이 아닌 재귀호출로 바꾼다면
+     * 시간복잡도가 O(n2)가 된다.
+     */
+
+    static boolean isMatch(HashMap<Integer[], Boolean> dp, int posW, int posS, String w, String s) {
+        if (dp.containsKey(new Integer[] { posW, posS }))
+            return dp.get(new Integer[] { posW, posW });
+        while (posS < s.length() && posW < w.length() && (w.charAt(posW) == '?' || w.charAt(posW) == s.charAt(posS))) {
+            posW++;
+            posS++;
+        }
+        // 2번 경우
+        if (posW == w.length()) {
+            Boolean result = posS == s.length();
+            dp.put(new Integer[] { posW, posS }, result);
+            return result;
+        }
+        if (w.charAt(posW) == '*')
+            for (int skip = 0; posS + skip <= s.length(); skip++) {
+                if (isMatch(dp, posW + 1, posS + skip, w, s)) {
+                    dp.put(new Integer[] { posW + 1, posS + skip }, true);
+                    return true;
+                }
+            }
+        dp.put(new Integer[] { posW, posS }, false);
         return false;
     }
 }
